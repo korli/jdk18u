bd47eca33b1fa0dc759a7f1cf93024893be47522
Jérôme Duval <jerome.duval@gmail.com>
2025-04-12 13:25:12 +0200

    Haiku patch from jdk17u

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -245,7 +245,7 @@ int os::create_file_for_heap(const char* dir) {
 
 static char* reserve_mmapped_memory(size_t bytes, char* requested_addr) {
   char * addr;
-  int flags = MAP_PRIVATE NOT_AIX( | MAP_NORESERVE ) | MAP_ANONYMOUS;
+  int flags = MAP_PRIVATE NOT_AIX( NOT_HAIKU( | MAP_NORESERVE )) | MAP_ANONYMOUS;
   if (requested_addr != NULL) {
     assert((uintptr_t)requested_addr % os::vm_page_size() == 0, "Requested address should be aligned to OS page size");
     flags |= MAP_FIXED;
@@ -476,7 +476,7 @@ void os::Posix::print_rlimit_info(outputStream* st) {
   st->print("%d", sysconf(_SC_CHILD_MAX));
 
   print_rlimit(st, ", THREADS", RLIMIT_THREADS);
-#else
+#elif !defined(HAIKU)
   print_rlimit(st, ", NPROC", RLIMIT_NPROC);
 #endif
 
@@ -1121,7 +1121,7 @@ bool os::Posix::handle_stack_overflow(JavaThread* thread, address addr, address 
                       "enabled executable stack (see man page execstack(8))");
 
   } else {
-#if !defined(AIX) && !defined(__APPLE__)
+#if !defined(AIX) && !defined(__APPLE__) && !defined(HAIKU)
     // bsd and aix don't have this
 
     // Accessing stack address below sp may cause SEGV if current
